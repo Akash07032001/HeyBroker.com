c10f2326eec2db6fa1fa61b9f7e8fb72d870fe47
Akash07032001 <ak9821531375@gmail.com>
2023-01-18 16:53:04 +0530

    Project structure created

---
+++ HeyBroker/src/main/java/com/masai/heybroker/HeyBrokerApplication.java
@@ -0,0 +1,13 @@
+package com.masai.heybroker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HeyBrokerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HeyBrokerApplication.class, args);
+    }
+
+}
---
+++ HeyBroker/src/test/java/com/masai/heybroker/HeyBrokerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.masai.heybroker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HeyBrokerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
