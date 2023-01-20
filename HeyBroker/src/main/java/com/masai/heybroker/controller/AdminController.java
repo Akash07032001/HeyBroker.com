422e62df34bdec6d83c48dd5076c6eb2922f80a7
Shashank Kumar <shashank9592sinha@gmail.com>
2023-01-20 21:06:40 +0530

    admin service implementation done

--- HeyBroker/src/main/java/com/masai/heybroker/controller/AdminController.java
+++ HeyBroker/src/main/java/com/masai/heybroker/controller/AdminController.java
@@ -1,11 +1,15 @@
 package com.masai.heybroker.controller;
 
 import com.masai.heybroker.model.Admin;
+import com.masai.heybroker.model.Broker;
+import com.masai.heybroker.model.Customer;
 import com.masai.heybroker.service.AdminService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/admin")
@@ -26,4 +30,57 @@ public class AdminController {
 
           return new ResponseEntity<Admin>(admin1,HttpStatus.OK);
     }
+
+    @DeleteMapping("/removecustomer/{key}/{cId}")
+    public ResponseEntity<Customer> removeCustomerHandler(@PathVariable String key , @PathVariable Integer cId){
+
+           Customer deletedCustomer =  adminService.removeCustomer(key,cId);
+            return new ResponseEntity<>(deletedCustomer, HttpStatus.OK);
+
+    }
+    @DeleteMapping("/removebroker/{key}/{bId}")
+    public ResponseEntity<Broker> removeBrokerHandler(@PathVariable String key, @PathVariable Integer bId){
+
+        Broker broker = adminService.removeBroker(key,bId);
+
+        return new ResponseEntity<>(broker, HttpStatus.OK);
+
+    }
+
+    @GetMapping("getallcustomer/{key}")
+    public ResponseEntity<List<Customer>> getAllCustomerHandler(@PathVariable String key){
+
+        List<Customer> customers = adminService.getAllCustomer(key);
+
+        return new ResponseEntity<>(customers, HttpStatus.OK);
+    }
+
+    @GetMapping("getallbroker/{key}")
+    public ResponseEntity<List<Broker>> getAllBrokerHandler(@PathVariable String key){
+
+        List<Broker> brokers = adminService.getAllBroker(key);
+
+        return new ResponseEntity<>(brokers, HttpStatus.OK);
+    }
+
+
+    @GetMapping("getcustomer/{key}/{cId}")
+    public ResponseEntity<Customer> getCustomerByIdHandler(@PathVariable String key , @PathVariable Integer cId){
+
+        Customer customer = adminService.getCustomerById(key,cId);
+
+        return new ResponseEntity<>(customer, HttpStatus.OK);
+
+    }
+
+    @GetMapping("getbroker/{key}/{bId}")
+    public ResponseEntity<Broker> getBrokerByIdHandler(@PathVariable String key , @PathVariable Integer bId){
+
+        Broker broker = adminService.getBrokerById(key,bId);
+
+        return new ResponseEntity<>(broker, HttpStatus.OK);
+
+    }
+
+
 }
--- HeyBroker/src/main/java/com/masai/heybroker/service/AdminServiceImpl.java
+++ HeyBroker/src/main/java/com/masai/heybroker/service/AdminServiceImpl.java
@@ -1,12 +1,21 @@
 package com.masai.heybroker.service;
 
 import com.masai.heybroker.exception.AdminException;
+import com.masai.heybroker.exception.BrokerException;
+import com.masai.heybroker.exception.CustomerException;
 import com.masai.heybroker.model.Admin;
 import com.masai.heybroker.model.AdminCurrentSesssion;
+import com.masai.heybroker.model.Broker;
+import com.masai.heybroker.model.Customer;
 import com.masai.heybroker.repository.AdminDao;
 import com.masai.heybroker.repository.AdminSessionDao;
+import com.masai.heybroker.repository.BrokerDao;
+import com.masai.heybroker.repository.CustomerDao;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class AdminServiceImpl implements AdminService{
@@ -14,6 +23,12 @@ public class AdminServiceImpl implements AdminService{
     private AdminDao adminDao;
     @Autowired
     private AdminSessionDao adminSessionDao;
+
+    @Autowired
+    private CustomerDao customerDao;
+
+    @Autowired
+    private BrokerDao brokerDao;
     @Override
     public Admin createAdmin(Admin admin) throws AdminException {
         Admin existingAdmin = adminDao.findByEmail(admin.getEmail());
@@ -35,4 +50,114 @@ public class AdminServiceImpl implements AdminService{
             return adminDao.save(admin);
         }else throw new AdminException("Invalid admin details please log in first");
     }
+
+    @Override
+    public Customer removeCustomer(String key, Integer cId) throws CustomerException {
+
+        AdminCurrentSesssion adminCurrentSesssion = adminSessionDao.findByAid(key);
+
+        if(adminCurrentSesssion==null) throw new AdminException("Please login first");
+
+        Optional<Customer> opt = customerDao.findById(cId);
+
+        if(!opt.isPresent()){
+            throw new CustomerException("Customer does not exists with customer id : "+cId);
+        }
+
+        Customer customer = opt.get();
+        customerDao.delete(customer);
+        return customer;
+
+    }
+
+    @Override
+    public Broker removeBroker(String key, Integer bId) throws BrokerException {
+        AdminCurrentSesssion adminCurrentSesssion = adminSessionDao.findByAid(key);
+
+        if(adminCurrentSesssion==null) throw new AdminException("Please login first");
+
+        Optional<Broker> opt = brokerDao.findById(bId);
+
+        if(!opt.isPresent()){
+            throw new BrokerException("Broker does not exists with broker id : "+bId);
+        }
+
+        Broker broker = opt.get();
+
+        brokerDao.delete(broker);
+        return broker;
+
+    }
+
+    @Override
+    public List<Customer> getAllCustomer(String key) throws CustomerException {
+        AdminCurrentSesssion adminCurrentSesssion = adminSessionDao.findByAid(key);
+
+        if(adminCurrentSesssion==null) throw new AdminException("Please login first");
+
+
+        List<Customer> customers = customerDao.findAll();
+
+        if(customers.isEmpty()){
+            throw new CustomerException("No customer found");
+        }
+        return customers;
+    }
+
+    @Override
+    public List<Broker> getAllBroker(String key) throws BrokerException {
+
+        AdminCurrentSesssion adminCurrentSesssion = adminSessionDao.findByAid(key);
+
+        if(adminCurrentSesssion==null) throw new AdminException("Please login first");
+
+        List<Broker> brokers = brokerDao.findAll();
+
+        if(brokers.isEmpty()){
+            throw new BrokerException("No broker found");
+        }
+
+        return brokers;
+
+    }
+
+    @Override
+    public Customer getCustomerById(String key , Integer cId) throws CustomerException {
+       AdminCurrentSesssion adminCurrentSesssion = adminSessionDao.findByAid(key);
+
+       if(adminCurrentSesssion==null) throw new AdminException("Please login first");
+
+       Optional<Customer> opt =  customerDao.findById(cId);
+
+       if (!opt.isPresent()){
+           throw new CustomerException("Customer does not exists wuth customer id : "+cId);
+       }
+
+       Customer customer  = opt.get();
+
+       return customer;
+
+    }
+
+
+    @Override
+    public Broker getBrokerById(String key , Integer bId) throws BrokerException {
+        AdminCurrentSesssion adminCurrentSesssion = adminSessionDao.findByAid(key);
+
+        if(adminCurrentSesssion==null) throw new AdminException("Please login first");
+
+        Optional<Broker> opt = brokerDao.findById(bId);
+
+        if (!opt.isPresent()){
+            throw new BrokerException("Broker does not exists with broker id : "+bId);
+
+        }
+
+        Broker broker = opt.get();
+
+        return broker;
+
+    }
+
+
 }
